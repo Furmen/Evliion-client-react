a843bb02085407ea22267f7414acdb4a29df120e
dfourmentel <dfourmentel@451.com>
2020-07-16 14:03:59 -0300

    Changes on page Verify Identity.

--- evliion-app-client/src/user/verifyIdentity/verifyIdentity.js
+++ evliion-app-client/src/user/verifyIdentity/verifyIdentity.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./verifyIdentity.css";
-import { Form, Input, Typography, Button } from "antd";
+import { Form, Input, Typography, Button, Select } from "antd";
 import { CountryDropdown } from 'react-country-region-selector'
 import { CLAIM_USER } from '../../constants';
 
@@ -8,12 +8,12 @@ const FormItem = Form.Item;
 const { Title } = Typography;
 const uuidv4 = require("uuid/v4");
 
+var types = ['Document-Passport', 'Identification Document', 'Driving Licence'];
+
 class VerifyIdentity extends Component {
   state = {
     country: "",
-    passport: "",
-    identificationCard: "",
-    driverLicence: ""
+    type: ""
   };
 
   constructor(props) {
@@ -21,27 +21,15 @@ class VerifyIdentity extends Component {
   }
 
   isFormInvalid() {
-    return !(this.state.passport.validateStatus === 'success' &&
-        this.state.identificationCard.validateStatus === 'success' &&
-        this.state.driverLicence.validateStatus === 'success' &&
-        this.state.country !== "");
+    return !(this.state.type !== "" && this.state.country !== "");
   }
 
   selectCountry (val) {
     this.setState({ country: val });
   }
 
-  handleInputChange(event, validationFun) {
-    const target = event.target;
-    const inputName = target.name;        
-    const inputValue = target.value;
-
-    this.setState({
-        [inputName] : {
-            value: inputValue,
-            ...validationFun(inputValue)
-        }
-    });
+  selectType = value => {
+    this.setState({type: value});
   }
 
   sendAPIRequest = () => {
@@ -49,9 +37,7 @@ class VerifyIdentity extends Component {
     const verifyData = {
       user_id: currentUser.id,
       country: this.state.country,
-      passport: this.state.passport.value,
-      identificationCard: this.state.identificationCard.value,
-      driverLicence: this.state.driverLicence.value
+      type: this.state.passport.value
     }
   };
 
@@ -68,47 +54,17 @@ class VerifyIdentity extends Component {
                   value={this.state.country}
                   onChange={(val) => this.selectCountry(val)} />
               </FormItem>
-              <br /><br />
-              <Title level={3}>Type</Title>
-              <br />
               <FormItem 
-                label="Passport"
-                hasFeedback
-                validateStatus={this.state.passport.validateStatus}
-                help={this.state.passport.errorMsg}>
-                <Input 
-                  size="large"
-                  name="passport"
-                  autoComplete="off"
-                  placeholder="Passport"
-                  value={this.state.passport.value} 
-                  onChange={(event) => this.handleInputChange(event, this.validatePassport)} />
-              </FormItem>
-              <FormItem 
-                label="Identification card"
-                hasFeedback
-                validateStatus={this.state.identificationCard.validateStatus}
-                help={this.state.identificationCard.errorMsg}>
-                <Input 
-                  size="large"
-                  name="identificationCard"
-                  autoComplete="off"
-                  placeholder="Identification card"
-                  value={this.state.identificationCard.value} 
-                  onChange={(event) => this.handleInputChange(event, this.validateIdentificationCard)} />
-              </FormItem>
-              <FormItem 
-                label="Driver's licence"
-                hasFeedback
-                validateStatus={this.state.driverLicence.validateStatus}
-                help={this.state.driverLicence.errorMsg}>
-                <Input 
-                  size="large"
-                  name="driverLicence"
-                  autoComplete="off"
-                  placeholder="Driver's Licence"
-                  value={this.state.driverLicence.value} 
-                  onChange={(event) => this.handleInputChange(event, this.validateDriverLicence)} />
+                label="Type">
+                  <Select
+                    size="large"
+                    onChange={this.selectType}>
+                      {types.map(e => (
+                        <Select.Option value={e} key={uuidv4()}>
+                          {e}
+                        </Select.Option>
+                      ))}
+                  </Select>
               </FormItem>
               <FormItem>
                 <Button type="primary" 
@@ -123,48 +79,6 @@ class VerifyIdentity extends Component {
           </Form>
       </div>
     );
-  }
-
-    validatePassport = (value) => {
-    if(!value || value.length === 0) {
-        return {
-            validateStatus: 'error',
-            errorMsg: "Passport is required"
-        }
-    } else {
-        return {
-            validateStatus: 'success',
-            errorMsg: null,
-        };            
-    }
-  }
-
-  validateIdentificationCard = (value) => {
-    if(!value || value.length === 0) {
-        return {
-            validateStatus: 'error',
-            errorMsg: "Identification Card is required"
-        }
-    } else {
-        return {
-            validateStatus: 'success',
-            errorMsg: null,
-        };            
-    }
-  }
-
-  validateDriverLicence = (value) => {
-    if(!value || value.length === 0) {
-        return {
-            validateStatus: 'error',
-            errorMsg: "Driver's Licence is required"
-        }
-    } else {
-        return {
-            validateStatus: 'success',
-            errorMsg: null,
-        };            
-    }
   }
 }
 export default VerifyIdentity;
